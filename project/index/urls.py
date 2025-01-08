bab5d3a4441fe823cd76eecfa153802a90cf6e14
Efremov-Denis <ruzs.efremov@mail.ru>
2025-01-08 23:45:04 +0300

    Added the ability to create a task

--- project/index/urls.py
+++ project/index/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='task_list'),  # URL для получения списка задач
     path('create/', views.TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/', views.TaskDetailView.as_view(), name='task_detail'),
+    path('index/<int:pk>/update/', views.TaskUpdateView.as_view(), name='task_update'),  # URL для обновления задачи
+    path('index/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task_delete'),  # URL для удаления задачи
 ]
